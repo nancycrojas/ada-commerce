cc9598d51441d1671ef90b5f939b2f7b19bc4626
nancycrojas <nanclarisaok@gmail.com>
2023-05-28 03:16:13 -0300

    Add state and function to handle password visibility toggle

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -7,13 +7,18 @@ import {
   FormErrorMessage,
   FormLabel,
   Heading,
+  IconButton,
   Input,
+  InputGroup,
+  InputRightElement,
   Link,
   SimpleGrid,
   Text,
   useToast,
 } from '@chakra-ui/react'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai'
 import { FcGoogle } from 'react-icons/fc'
 import { NavLink, useNavigate } from 'react-router-dom'
 
@@ -25,6 +30,9 @@ export const Login = () => {
   const { errors, isSubmitting } = formState
   const navigate = useNavigate()
   const toast = useToast()
+
+  const [show, setShow] = useState(false)
+  const handleClick = () => setShow(!show)
 
   const onSubmit = async (data) => {
     try {
@@ -89,88 +97,105 @@ export const Login = () => {
       position="relative"
     >
       <Flex justifyContent="center" align="center" p={2}>
-        <Button
-          position="absolute"
-          top="4"
-          left="4"
-          as={NavLink}
-          fontWeight="semibold"
-          to="/"
-          _hover={{ color: '#BE3969' }}
-        >
-          Volver
-        </Button>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Heading as="h1" mb={4} color="#BE3969" textAlign="center">
-            Iniciar Sesión
-          </Heading>
-          <Button type="button" mb={4} w="full" onClick={handleLoginWithGoogle}>
-            <Link mr={2}>
-              <FcGoogle size={20} />
-            </Link>
-            Continuar con Google
-          </Button>
-          <Flex
-            justifyContent="center"
-            alignItems="center"
-            gap={2}
-            textAlign="center"
+        <Box w="full" maxW="350px">
+          <Button
+            position="absolute"
+            top="4"
+            left="4"
+            as={NavLink}
+            fontWeight="semibold"
+            to="/"
+            _hover={{ color: '#BE3969' }}
           >
-            <Divider flex={1} />
-            <Text fontSize="sm" fontWeight="light" color="gray" mb={2}>
-              O inicia sesión con tu correo electrónico
-            </Text>
-            <Divider flex={1} />
-          </Flex>
-          <FormControl isInvalid={errors.email}>
-            <FormLabel>Email</FormLabel>
-            <Input
-              type="email"
-              name="email"
-              id="email"
-              autoComplete="off"
-              {...register('email', {
-                required: 'Este campo es requerido',
-                pattern: {
-                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                  message: 'Este email no es válido',
-                },
-              })}
-            />
-            <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
-          </FormControl>
-          <FormControl isInvalid={errors.password}>
-            <FormLabel mt={2}>Contraseña</FormLabel>
-            <Input
-              type="password"
-              name="password"
-              id="password"
-              autoComplete="off"
-              {...register('password', {
-                required: 'Este campo es requerido',
-                minLength: {
-                  value: 6,
-                  message: 'El mínimo de caracteres es 6',
-                },
-              })}
-            />
-            <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
-            <Button type="submit" mt={4} w="full" isLoading={isSubmitting}>
+            Volver
+          </Button>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Heading as="h1" mb={4} color="#BE3969" textAlign="center">
               Iniciar Sesión
-            </Button>
-            <Flex justifyContent="center" mt={3} gap={2}>
-              <Text>¿No tenes cuenta? </Text>
-              <Link
-                as={NavLink}
-                fontWeight="semibold"
-                to="/registro"
-                _hover={{ color: '#BE3969' }}
-              >
-                Regístrate
+            </Heading>
+            <Button
+              type="button"
+              mb={4}
+              w="full"
+              onClick={handleLoginWithGoogle}
+            >
+              <Link mr={2}>
+                <FcGoogle size={20} />
               </Link>
+              Continuar con Google
+            </Button>
+            <Flex
+              justifyContent="center"
+              alignItems="center"
+              gap={2}
+              textAlign="center"
+            >
+              <Divider flex={1} />
+              <Text fontSize="sm" fontWeight="light" color="gray" mb={2}>
+                O inicia sesión con tu correo electrónico
+              </Text>
+              <Divider flex={1} />
             </Flex>
-          </FormControl>
-        </form>
+            <FormControl isInvalid={errors.email}>
+              <FormLabel>Email</FormLabel>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                autoComplete="off"
+                {...register('email', {
+                  required: 'Este campo es requerido',
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: 'Este email no es válido',
+                  },
+                })}
+              />
+              <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+            </FormControl>
+            <FormControl isInvalid={errors.password}>
+              <FormLabel mt={2}>Contraseña</FormLabel>
+              <InputGroup>
+                <Input
+                  type={show ? 'text' : 'password'}
+                  name="password"
+                  id="password"
+                  autoComplete="off"
+                  {...register('password', {
+                    required: 'Este campo es requerido',
+                    minLength: {
+                      value: 6,
+                      message: 'El mínimo de caracteres es 6',
+                    },
+                  })}
+                />
+                <InputRightElement>
+                  <IconButton
+                    icon={show ? <AiFillEye /> : <AiFillEyeInvisible />}
+                    variant="outline"
+                    border="none"
+                    onClick={handleClick}
+                  />
+                </InputRightElement>
+              </InputGroup>
+              <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+              <Button type="submit" mt={4} w="full" isLoading={isSubmitting}>
+                Iniciar Sesión
+              </Button>
+              <Flex justifyContent="center" mt={3} gap={2}>
+                <Text>¿No tenes cuenta? </Text>
+                <Link
+                  as={NavLink}
+                  fontWeight="semibold"
+                  to="/registro"
+                  _hover={{ color: '#BE3969' }}
+                >
+                  Regístrate
+                </Link>
+              </Flex>
+            </FormControl>
+          </form>
+        </Box>
       </Flex>
       <Box bg="gray.200" display={{ base: 'none', md: 'block' }} />
     </SimpleGrid>
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -6,13 +6,18 @@ import {
   FormErrorMessage,
   FormLabel,
   Heading,
+  IconButton,
   Input,
+  InputGroup,
+  InputRightElement,
   Link,
   SimpleGrid,
   Text,
   useToast,
 } from '@chakra-ui/react'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai'
 import { FcGoogle } from 'react-icons/fc'
 import { NavLink, useNavigate } from 'react-router-dom'
 
@@ -24,6 +29,9 @@ export const Register = () => {
   const { errors, isSubmitting } = formState
   const navigate = useNavigate()
   const toast = useToast()
+
+  const [show, setShow] = useState(false)
+  const handleClick = () => setShow(!show)
 
   const onSubmit = async (data) => {
     try {
@@ -80,70 +88,82 @@ export const Register = () => {
   return (
     <SimpleGrid h="100vh" templateColumns={{ base: '1fr', md: '1fr 1fr' }}>
       <Flex justifyContent="center" align="center" p={2}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Heading as="h1" mb={4} color="#BE3969" textAlign="center">
-            Crear cuenta
-          </Heading>
-          <Button
-            type="button"
-            mb={4}
-            w="full"
-            onClick={handleRegisterWithGoogle}
-          >
-            <Link mr={2}>
-              <FcGoogle size={20} />
-            </Link>
-            Continuar con Google
-          </Button>
-          <FormControl isInvalid={errors.email}>
-            <FormLabel>Email</FormLabel>
-            <Input
-              type="email"
-              name="email"
-              id="email"
-              autoComplete="off"
-              {...register('email', {
-                required: 'Este campo es requerido',
-                pattern: {
-                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                  message: 'Este email no es válido',
-                },
-              })}
-            />
-            <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
-          </FormControl>
-          <FormControl isInvalid={errors.password}>
-            <FormLabel mt={2}>Contraseña</FormLabel>
-            <Input
-              type="password"
-              name="password"
-              id="password"
-              autoComplete="off"
-              {...register('password', {
-                required: 'Este campo es requerido',
-                minLength: {
-                  value: 6,
-                  message: 'El mínimo de caracteres es 6',
-                },
-              })}
-            />
-            <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
-            <Button type="submit" mt={4} w="full" isLoading={isSubmitting}>
-              Crear Cuenta
-            </Button>
-            <Flex justifyContent="center" mt={3} gap={2}>
-              <Text>¿Ya tenes cuenta?</Text>
-              <Link
-                as={NavLink}
-                fontWeight="semibold"
-                to="/iniciar-sesion"
-                _hover={{ color: '#BE3969' }}
-              >
-                Iniciar Sesión
+        <Box w="full" maxW="350px">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Heading as="h1" mb={4} color="#BE3969" textAlign="center">
+              Crear cuenta
+            </Heading>
+            <Button
+              type="button"
+              mb={4}
+              w="full"
+              onClick={handleRegisterWithGoogle}
+            >
+              <Link mr={2}>
+                <FcGoogle size={20} />
               </Link>
-            </Flex>
-          </FormControl>
-        </form>
+              Continuar con Google
+            </Button>
+            <FormControl isInvalid={errors.email}>
+              <FormLabel>Email</FormLabel>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                autoComplete="off"
+                {...register('email', {
+                  required: 'Este campo es requerido',
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: 'Este email no es válido',
+                  },
+                })}
+              />
+              <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+            </FormControl>
+            <FormControl isInvalid={errors.password}>
+              <FormLabel mt={2}>Contraseña</FormLabel>
+              <InputGroup>
+                <Input
+                  type={show ? 'text' : 'password'}
+                  name="password"
+                  id="password"
+                  autoComplete="off"
+                  {...register('password', {
+                    required: 'Este campo es requerido',
+                    minLength: {
+                      value: 6,
+                      message: 'El mínimo de caracteres es 6',
+                    },
+                  })}
+                />
+                <InputRightElement>
+                  <IconButton
+                    icon={show ? <AiFillEye /> : <AiFillEyeInvisible />}
+                    variant="outline"
+                    border="none"
+                    onClick={handleClick}
+                  />
+                </InputRightElement>
+              </InputGroup>
+              <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+              <Button type="submit" mt={4} w="full" isLoading={isSubmitting}>
+                Crear Cuenta
+              </Button>
+              <Flex justifyContent="center" mt={3} gap={2}>
+                <Text>¿Ya tenes cuenta?</Text>
+                <Link
+                  as={NavLink}
+                  fontWeight="semibold"
+                  to="/iniciar-sesion"
+                  _hover={{ color: '#BE3969' }}
+                >
+                  Iniciar Sesión
+                </Link>
+              </Flex>
+            </FormControl>
+          </form>
+        </Box>
       </Flex>
       <Box bg="gray.200" display={{ base: 'none', md: 'block' }} />
     </SimpleGrid>
